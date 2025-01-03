7932e269130c9e8e599fb47e69fa312d31c586e7
mathoera@kirpalani.com <mathoera@kirpalani.com>
2025-01-03 06:47:50 -0300

    Solved UI issue where seed phrase was not properly displayed in chips. Solution: imported primereact css and installed primeflex.

--- su-wallet/src/App.tsx
+++ su-wallet/src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import { generateMnemonic} from 'bip39'
-import './App.css'
+import { generateMnemonic } from 'bip39'
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeflex/primeflex.css'; // flex
 import { PrimeReactProvider } from 'primereact/api'
 import { Card } from 'primereact/card';
 import { Chip } from 'primereact/chip';
@@ -13,13 +15,12 @@ function App() {
   return (
     
     <PrimeReactProvider>      
-      <Button label="Generate Seed Phrase" onClick={() => setMnemonic(generateMnemonic())}/>      
-      <Card title="Seed phrase">
-        {
+      <Button label="Generate Seed Phrase" onClick={() => { setMnemonic(generateMnemonic())}}/>      
+      <Card title="Seed phrase" className='bg-pink-700'>
+        {          
           <div className="card flex flex-wrap gap-2">
-            {mnemonic.split(' ').map((word, index) => (
-            
-              <Chip key={index} label={word} />
+            {mnemonic.split(' ').map((word, i) => (            
+              <Chip key={i} label={word} />
             ))}
         </div>
           
